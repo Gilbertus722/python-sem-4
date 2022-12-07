e99ff2b6c79f5542731241d729f9f7e100428f15
Vitalii <gilbertus722@yandex.ru>
2022-12-07 14:44:53 +0300

    002

--- DZ/DZ4-3.py
+++ DZ/DZ4-3.py
@@ -2,6 +2,24 @@
 # изменить на прописные буквы фамилии тех студентов, 
 # которые имеют средний балл более «4»
 
-open("DZ4-3.txt")
-print(file.read())
-file.close()
+
+
+from typing import List
+
+def get_list_data(filename: str) -> List[str]:
+    with open(filename, encoding='utf-8') as file:
+        return file.read().split('\n')
+    
+def elements_to_caps(lst: list, trigger_str: str) -> List[str]:
+    for i in range(len(lst)):
+        if trigger_str in lst[i]:
+            lst[i].upper()
+    return lst
+
+lst = get_list_data('G:\Учеба\python dz\seminar 4\DZ4\DZ\DZ4-3.txt')
+
+print(elements_to_caps(lst, '5'))
+
+with open('G:\Учеба\python dz\seminar 4\DZ4\DZ\DZ4-3.txt', 'w', encoding='utf-8') as data:
+    for i in range(len(lst)):
+        data.writelines(f'{lst[i]}\n')
--- DZ/DZ4-5.py
+++ DZ/DZ4-5.py
@@ -1,93 +1,40 @@
 #5 - Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных. 
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
-with open ('file_encode.txt', 'w') as data:
 
-    data.write(with open('file_encode.txt', 'w') as data:
+with open('enode.txt', 'r') as data:
+    my_text = data.read()
 
-    data.write('WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW')
-
-with open('file_encode.txt', 'r') as data:
-    string = data.readline()
-
-def rle_encode(decoded_string):
-    encoded_string = ''
+def encode_rle(ss):
+    str_code = ''
+    prev_char = ''
     count = 1
-    char = decoded_string[0]
-    for i in range(1, len(decoded_string)):
-        if decoded_string[i] == char:
-            count += 1
-        else:
-            encoded_string = encoded_string + str(count) + char
-            char = decoded_string[i]
+    for char in ss:
+        if char != prev_char:
+            if prev_char:
+                str_code += str(count) + prev_char
             count = 1
-            encoded_string = encoded_string + str(count) + char
-    return encoded_string
-
-
-def rle_decode(encoded_string):
-    decoded_string = ''
-    char_amount = ''
-    for i in range(len(encoded_string)):
-        if encoded_string[i].isdigit():
-            char_amount += encoded_string[i]
+            prev_char = char
         else:
-            decoded_string += encoded_string[i] * int(char_amount)
-        char_amount = ''
-    print(decoded_string)
-
-    return decoded_string
-
-
-with open('file_encode.txt', 'r') as file:
-    decoded_string = file.read()
-
-with open('file_decode.txt', 'w') as file:
-    encoded_string = rle_encode(decoded_string)
-    file.write(encoded_string)
-
-print('До сжатия: \t' + decoded_string)
-print('После сжатия: \t' + rle_encode(decoded_string))
-print(f'Кофициент сжатия: \t{round(len(decoded_string) / len(encoded_string), 1)}'))
-
-with open('file_encode.txt', 'r') as data:
-    string = data.readline()
-
-def rle_encode(decoded_string):
-    encoded_string = ''
-    count = 1
-    char = decoded_string[0]
-    for i in range(1, len(decoded_string)):
-        if decoded_string[i] == char:
             count += 1
+    return str_code
+
+            
+str_code = encode_rle(my_text)
+print(str_code)
+
+with open('szhato.txt', 'r') as data:
+    my_text2 = data.read()
+
+def decoding_rle(ss:str):
+    count = ''
+    str_decode = ''
+    for char in ss:
+        if char.isdigit():
+            count += char 
         else:
-            encoded_string = encoded_string + str(count) + char
-            char = decoded_string[i]
-            count = 1
-            encoded_string = encoded_string + str(count) + char
-    return encoded_string
+            str_decode += char * int(count)
+            count = ''
+    return str_decode
 
-
-def rle_decode(encoded_string):
-    decoded_string = ''
-    char_amount = ''
-    for i in range(len(encoded_string)):
-        if encoded_string[i].isdigit():
-            char_amount += encoded_string[i]
-        else:
-            decoded_string += encoded_string[i] * int(char_amount)
-        char_amount = ''
-    print(decoded_string)
-
-    return decoded_string
-
-
-with open('file_encode.txt', 'r') as file:
-    decoded_string = file.read()
-
-with open('file_decode.txt', 'w') as file:
-    encoded_string = rle_encode(decoded_string)
-    file.write(encoded_string)
-
-print('До сжатия: \t' + decoded_string)
-print('После сжатия: \t' + rle_encode(decoded_string))
-print(f'Кофициент сжатия: \t{round(len(decoded_string) / len(encoded_string), 1)}')
+str_decode = decoding_rle(my_text2)
+print(str_decode)
